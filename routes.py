520e0f61103dbb4e59001ff5bc8e56b8da7ef9cd
nixalpha <n1xalphaa@gmail.com>
2023-04-02 22:19:53 +0000

    new endpoint

--- routes.py
+++ routes.py
@@ -9,6 +9,12 @@ from langchain.schema import (
     SystemMessage
 )
 
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field, validator
+
+from typing import List
+
 import os
 from dotenv import load_dotenv
 
@@ -25,9 +31,34 @@ def query(input_text):
         HumanMessage(content=input_text)
     ]
 
-    response = chatgpt(messages)
+    resp = chatgpt(messages)
 
-    return response.content
+    return resp.content
+
+class SustPlantInfo(BaseModel):
+  common_name: str = Field(description="The common name to a sustainable plant.")
+  sci_name: str = Field(description="The plant's scientific name.")
+  desc: str = Field(description="A 2-3 sentence description of the plant and why it is good for a garden.")
+
+class SustPlants(BaseModel):
+  plants: List[SustPlantInfo]
+
+def query_sustplants(num):
+    plant_query = "Give me" + num + "sustainable plants to plant in California in April."
+    parser = PydanticOutputParser(pydantic_object=SustPlants)
+
+    prompt = PromptTemplate(
+        template="Answer the user query.\n{format_instructions}\n{query}\n",
+        input_variables=["query"],
+        partial_variables={"format_instructions": parser.get_format_instructions()}
+    )
+
+    _input = prompt.format_prompt(query=plant_query)
+
+    model = ChatOpenAI(model_name='gpt-3.5-turbo')
+    resp = model([HumanMessage(content=_input.text)])
+
+    return resp.content
 
 @app.route('/completion', methods=['GET', 'POST'])
 def completion_api():
@@ -37,6 +68,12 @@ def completion_api():
         return Response(query(input_text), mimetype='text/plain')
     else:
         return Response(None, mimetype='text/plain')
+    
+@app.route('/sustplants', methods=['POST'])
+def sust_plants():
+    data = request.form
+    num = data['num']
+    return Response(query_sustplants(num), mimetype='text/plain')
     
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0')
